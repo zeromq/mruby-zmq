2181f1ce718c559ec46cebc0cd1920951df5ec2c
Asmod4n <Asmod4n@users.noreply.github.com>
2017-06-28 23:11:16 +0200

    build in travis with MRB_USE_ETEXT_EDATA

--- build_config.rb
+++ build_config.rb
@@ -6,4 +6,6 @@ MRuby::Build.new do |conf|
   conf.gem File.expand_path(File.dirname(__FILE__))
   conf.cc.defines << 'MRB_INT64'
   conf.cxx.defines << 'MRB_INT64'
+  conf.cc.defines << 'MRB_USE_ETEXT_EDATA'
+  conf.cxx.defines << 'MRB_USE_ETEXT_EDATA'
 end
