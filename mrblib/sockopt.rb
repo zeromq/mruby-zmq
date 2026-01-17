83b1c943c36b1877849b805cbd530c404b1d9f2a
Asmod4n <791770+Asmod4n@users.noreply.github.com>
2026-01-17 10:23:31 +0100

    expose ZMQ_CONNECT_ROUTING_ID

--- mrblib/sockopt.rb
+++ mrblib/sockopt.rb
@@ -115,7 +115,7 @@ module ZMQ
       end
     end
 
-    ["connect_rid", "curve_publickey", "curve_secretkey", "curve_serverkey", "gssapi_principal", "gssapi_service_principal", "plain_password",
+    ["connect_rid", "connect_routing_id", "curve_publickey", "curve_secretkey", "curve_serverkey", "gssapi_principal", "gssapi_service_principal", "plain_password",
       "plain_username", "zap_domain"].each do |data|
         updata = data.upcase
       if LibZMQ.const_defined?(updata)
